c36ca403255fd439c842c45fd9bfb19eb7044e71
Aaron Caldwell <arcaldwell49@gmail.com>
2020-04-22 09:45:36 -0400

    Revert "Revert "Quick fixes""
    This reverts commit 4b6b637ceab9e07b28f7b0efbdedc36639e4a430.

--- R/emmeans_power.R
+++ R/emmeans_power.R
@@ -4,7 +4,8 @@
 #' \emph{Do not use to calculate "observed power" for empirical datasets
 #' (Hoenig & Heisey, 2001).}
 #'
-#' @param x \code{emmGrid}. Grid of contrasts to 
+#' @param x \code{emmGrid}. Grid of contrasts to estimate power from.
+#' @param ... Other arguments passed to the function if object is not already a \code{emmGrid} object.
 #' @inheritParams ANOVA_exact 
 #' @details Note that calculation of power is based on the F- and t-ratio assuming
 #'   two-sided testing. Thus, the function does not honor adjustments of the
@@ -16,6 +17,7 @@
 #'   dataset are consistent with the directional hypothesis, by doubeling
 #'   \code{alpha_level}. Similarly, power for Bonferroni-corrected contrasts can be
 #'   calculated by adjusting \code{alpha_level} accordingly (see examples).
+#'   \code{...} Other arugments passed onto the function
 #'
 #' @inherit ANOVA_exact return
 #' @importFrom magrittr '%>%'
@@ -25,12 +27,17 @@
 #' @references Hoenig, J. M., & Heisey, D. M. (2001). The Abuse of Power. The American Statistician, 55(1), 19–24. https://doi.org/10.1198/000313001300339897
 #' @export
 #' @examples
-#' 
+#' \dontrun{
 #' # Set up a within design with 2 factors, each with 2 levels
-#' design_result <- ANOVA_design(design = "2w*2w", n = 40, mu = c(1, 0, 1, 0),
-#'                               sd = 2, r = 0.8, labelnames = c("condition", "cheerful",
-#'                                                               "sad", "voice", "human", "robot"))
-#' exact_result <- ANOVA_exact(design_result, alpha_level = 0.05, verbose = FALSE, emm = TRUE, #' contrast_type = "pairwise")
+#' design_result <- ANOVA_design(design = "2w*2w",
+#' n = 40, mu = c(1, 0, 1, 0),
+#' sd = 2, r = 0.8,
+#' labelnames = c("condition", "cheerful",
+#'  "sad", "voice", "human", "robot"))
+#'  
+#' exact_result <- ANOVA_exact(design_result, 
+#' alpha_level = 0.05, verbose = FALSE, 
+#' emm = TRUE, contrast_type = "pairwise")
 #' 
 #' # Power for pairwise contrasts
 #' exact_result$emm_results
@@ -39,21 +46,30 @@
 #' exact_result$emmeans$contrasts
 #' 
 #' # Manually recalculate power
-#' emmeans_power(exact_result$emmeans$contrasts, alpha_level = 0.05)
+#' emmeans_power(exact_result$emmeans$contrasts, 
+#' alpha_level = 0.05)
 #' 
 #' # Calculate power for Bonferroni-adjusted pairwise comparisons
 #' n_contrasts <- nrow(as.data.frame(exact_result$emmeans$contrasts))
-#' emmeans_power(exact_result$emmeans$contrasts, alpha_level = 0.05 / n_contrasts)
+#' emmeans_power(exact_result$emmeans$contrasts, 
+#' alpha_level = 0.05 / n_contrasts)
 #' 
 #' # Calculate power for one-sided custom contrasts
 #' exact_result$emmeans$emmeans
-#' custom_contrast <- contrast(exact_result$emmeans$emmeans, list(robot_vs_sad_human = c(0, 1, -0.5, #' -0.5)))
-#' emmeans_power(custom_contrast, alpha_level = 0.05 * 2)
+#' custom_contrast <- contrast(exact_result$emmeans$emmeans, 
+#' list(robot_vs_sad_human = c(0, 1, -0.5, -0.5)))
+#' emmeans_power(custom_contrast,
+#'  alpha_level = 0.05 * 2)
 #' 
 #' # Calculate power for follow-up ANOVA
-#' follow_up <- joint_tests(exact_result$emmeans$emmeans, by = "condition")
-#' emmeans_power(follow_up, alpha_level = 0.05 / 2)
-#' emmeans_power(emmeans(exact_result$emmeans$emmeans, pairwise ~ voice | condition)$contrasts, #' alpha_level = 0.05 / 2)
+#' follow_up <- joint_tests(exact_result$emmeans$emmeans, 
+#' by = "condition")
+#' emmeans_power(follow_up, 
+#' alpha_level = 0.05 / 2)
+#' emmeans_power(emmeans(exact_result$emmeans$emmeans, 
+#' pairwise ~ voice | condition)$contrasts, 
+#' alpha_level = 0.05 / 2)
+#' }
 
 emmeans_power <- function(x, ...) {
   UseMethod("emmeans_power", x)
@@ -77,12 +93,12 @@ emmeans_power.summary_em <- function(x, ...) {
 #' @rdname emmeans_power
 #' @export
 
-emmeans_power.data.frame <- function(x, alpha_level = Superpower_options("alpha_level")) {
-  if(is.null(x$t.ratio) & is.null(x$F.ratio)) stop("emmGrid object 'x' does not contain test statistic (t or F). Please set `infer = TRUE` in emmeans.")
+emmeans_power.data.frame <- function(x, alpha_level = Superpower_options("alpha_level"), ...) {
+  if (is.null(x$t.ratio) & is.null(x$F.ratio)) stop("emmGrid object 'x' does not contain test statistic (t or F). Please set `infer = TRUE` in emmeans.")
   
   # Need for exact; not necessary for power function
   # Convert t-ratio to F-stat
-  if(is.null(x$F.ratio)) {
+  if (is.null(x$F.ratio)) {
     x$F.ratio <- (x$t.ratio)^2
     x$df1 <- 1
     x$df2 <- x$df
