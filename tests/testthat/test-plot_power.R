dbd0f921b79893bfb10fcf8ec45824e80c6f0494
Aaron Caldwell <arcaldwell49@gmail.com>
2020-05-13 11:35:23 -0400

    Start of fix
    plot_power now broken. working on fix of Daniels edits

--- tests/testthat/test-plot_power.R
+++ tests/testthat/test-plot_power.R
@@ -15,7 +15,7 @@ test_that("error messages", {
                          mu = c(0,0,0,0,0.5,0.5,0.5,0.5),
                          sd = 1,
                          plot = FALSE)
-
+  
   expect_error(plot_power(), "argument \"design_result\" is missing, with no default")
   expect_error(plot_power(design),
                "plot_power must have an ANOVA_design object with n > the product of the factors; please increase the n in ANOVA_design function.",
@@ -28,23 +28,19 @@ test_that("test multi functions", {
                          mu = c(0,0,0,0,0.5,0.5,0.5,0.5),
                          sd = 1,
                          plot = FALSE)
-  p <- hush(plot_power(design, emm = TRUE, min_n = 7, max_n = 100,
-                  verbose = TRUE))
-  design_ex1 <- ANOVA_design(design = "2b*4w",
-                         n = 70,
-                         mu = c(0,0,0,0,0.5,0.5,0.5,0.5),
-                         sd = 1,
-                         plot = FALSE)
+  
+  p <- hush(plot_power(design, emm = TRUE, min_n = 8, max_n = 100))
+  
   p_ex1 = ANOVA_exact(design, emm = TRUE, verbose = FALSE)
   
   res70 = p$power_df_emm[p$power_df_emm$n == 70, ]
   res70 = as.numeric(res70[,-1])
   expect_setequal(round(res70,1), round(p_ex1$emm_results$power,1))
-  expect_equal(p$power_df_manova[1,3],36.2,tolerance = .01)
-  expect_equal(p$power_df_manova[1,2],36.2,tolerance = .01)
+  expect_equal(p$power_df_manova[1,3],41.9,tolerance = .01)
+  expect_equal(p$power_df_manova[1,2],41.9,tolerance = .01)
   expect_equal(p$power_df_manova[1,4],5,tolerance = .01)
   expect_equal(p$power_df_manova[1,5],5,tolerance = .01)
-  expect_equal(p$power_df_manova[94,3],99.9,tolerance = .01)
+  expect_equal(p$power_df_manova[93,3],99.9,tolerance = .01)
 })
 
 
@@ -56,7 +52,7 @@ test_that("test 2b", {
                          plot = FALSE)
   
   p = hush(plot_power(design, emm = TRUE, min_n = 7, max_n = 100,
-                 verbose = TRUE))
+                      verbose = TRUE))
   
   p_ex1 = ANOVA_exact(design, emm = TRUE, verbose = FALSE)
   res7 = p$power_df_emm[p$power_df_emm$n == 7, ]
@@ -65,4 +61,40 @@ test_that("test 2b", {
   expect_equal(p$power_df[1,2], 13.8,tolerance = .05)
   expect_equal(p$power_df[94,2],96,tolerance = .01)
   
+})
+
+
+test_that("match",{
+  design <- ANOVA_design(design = "3b*2b",
+                         n = 20,
+                         mu = c(1, 2, 2, 3, 3, 4),
+                         sd = 3,
+                         plot = FALSE)
+  x1 = ANOVA_exact(design, verbose = FALSE)
+  p1 = plot_power(design,
+                  min_n = 10, max_n = 100,
+                  desired_power = 90, plot = FALSE,
+                  verbose = FALSE)
+  design2 <- ANOVA_design(design = "3b*2b",
+                          n = 80,
+                          mu = c(1, 2, 2, 3, 3, 4),
+                          sd = 3,
+                          plot = FALSE)
+  x2 = ANOVA_exact(design2, verbose = FALSE)
+  p2 = plot_power(design2,
+                  min_n = 10, max_n = 100,
+                  desired_power = 90, plot = FALSE,
+                  verbose = FALSE)
+  
+  design3 <- ANOVA_design(design = "3b*2b",
+                          n = 180,
+                          mu = c(1, 2, 2, 3, 3, 4),
+                          sd = 3,
+                          plot = FALSE)
+  x3 = ANOVA_exact(design3, verbose = FALSE)
+  expect_setequal(round(p1$power_df$b,2),round(p2$power_df$b,2))
+  
+  expect_setequal(round(p1$power_df$a,2),round(p2$power_df$a,2))
+  
+  expect_setequal(round(p1$power_df$`a:b`,2),round(p2$power_df$`a:b`,2))
 })
