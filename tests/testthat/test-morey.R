eee3a45f5f44adbce59f1f06293977e571194b0e
Aaron Caldwell <arcaldwell49@gmail.com>
2025-08-22 08:30:25 -0500

    update test for morey plot

--- tests/testthat/test-morey.R
+++ tests/testthat/test-morey.R
@@ -106,6 +106,6 @@ test_that("No error messages ftest",{
                           alpha_level = .055,
                           liberal_lambda = TRUE)
   
-  expect_equal(class(test)[1], "gg")
+  expect_equal(class(test)[1], "ggplot2::ggplot")
   
 })
